40d7e3aee58711385dda7a06a77e5773d81c892f
yyang2018mel <michael.yangyifan@gmail.com>
2022-12-23 21:26:56 +1100

    Classes, Interfaces and OOP - $19 Exercise

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,48 @@
-// abstract class Calendar 
-// {
-//     constructor(public name: string) 
-//     { }
-
-//     abstract addEvent() : void;
-//     abstract removeEvent(): void
-// }
-
-
-interface Calendar
+class Logger
 {
-    name: string;
-    addEvent(): void;
-    removeEvent(): void;
-}
+    private _fileName: string;
 
-interface CloudCalendar extends Calendar
-{
-    sync(): void;
-}
-
-class GoogleCalendar implements Calendar
-{
-    constructor(public name: string) 
-    { }
-
-    addEvent(): void 
+    constructor(fileName: string) 
     {
-        throw new Error("Method not implemented.");
+        this._fileName = fileName;
     }
-    removeEvent(): void 
+
+    Log() : void
     {
-        throw new Error("Method not implemented.");
+        console.log('Logging to ' + this._fileName);
     }
-    
 }
+
+class Person
+{
+    constructor(public firstName: string, public lastName: string) 
+    {
+    }
+
+    get fullName()
+    {
+        return this.firstName + ' ' + this.lastName;
+    }
+}
+
+class Employee extends Person
+{
+    constructor(firstName: string, lastName: string, public salary: number) 
+    {
+        super(firstName, lastName);    
+    }
+}
+
+type Address = 
+{
+    street: string,
+    city: string,
+    zipCode: number
+};
+
+interface IEmployee
+{
+    name: string,
+    salary: number,
+    address: Address
+};
